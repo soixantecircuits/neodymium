521bd388244e3fe03c010c464c06f642a46f7275
hugohil <hugo@soixantecircuits.fr>
2016-01-28 16:31:04 +0100

    Handle multiple route for a same controller

--- app/templates/app/scripts/lib/router.js
+++ app/templates/app/scripts/lib/router.js
@@ -7,8 +7,14 @@ module.exports = (function router () {
   let self = {}
 
   const paths = [
-    'home',
-    'page'
+    {
+      routes: ['home'],
+      template: 'home'
+    },
+    {
+      routes: ['page-1', 'page-2'],
+      template: 'page'
+    }
   ]
 
   let controllers = {}
@@ -17,8 +23,11 @@ module.exports = (function router () {
 
   for (let i = 0; i < paths.length; i++) {
     let path = paths[i]
-    controllers[path] = require('../controllers/' + path)
-    views[path] = require('../../partials/' + path + '.html')
+    for (let j = 0; j < path.routes.length; j++) {
+      let route = path.routes[j]
+      controllers[route] = require('../controllers/' + path.template)
+      views[route] = require('../../partials/' + path.template + '.html')
+    }
   }
 
   self.init = function init () {
